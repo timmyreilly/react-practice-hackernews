4f78e464e78de94818d899eb2be3f5af86074b28
Tim Reilly <tireilly@microsoft.com>
2018-01-26 15:33:16 -0800

    table and button as stateless functional components

--- src/App.js
+++ src/App.js
@@ -30,66 +30,48 @@ function isSearched(searchTerm) {
   // item.title.toLowerCase().includes(searchTerm.toLowerCase()); 
 }
 
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
 
-function SearchTwo({value, onChange, children}) {
+
+const Button = ({ onClick, className = '', children }) => {
+  // do something 
   return (
-    <form>
-      {children} < input
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
-    </form>
-  );
-}
-
-const Search = ({ value, onChange, children }) => 
-<form> 
-  {children} <input 
-    type="text"
-    value={value}
-    onChange={onChange}
-  />
-</form>
-
-
-function Button(props){
-  const { onClick, className = '', children } = props; 
-
-  return (
-    <button 
+    <button
       onClick={onClick}
       className={className}
       type="button">
       {children}
-    </button> 
-  );
+    </button>
+  )
 }
 
-function Table(props){
-  const { list, pattern, onDismiss } = props; 
 
-  return (
-    <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author} </span>
-            <span>{item.num_comments} </span>
-            <span>{item.points} </span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        )}
-      </div>
-  ); 
-}
+const Table =({ list, pattern, onDismiss }) =>
+<div>
+  {list.filter(isSearched(pattern)).map(item =>
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author} </span>
+      <span>{item.num_comments} </span>
+      <span>{item.points} </span>
+      <span>
+        <Button onClick={() => onDismiss(item.objectID)}>
+          Dismiss
+          </Button>
+      </span>
+    </div>
+  )}
+</div>
 
 
 
